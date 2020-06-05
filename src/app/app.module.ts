449704e8e26e329afbf205e6631d75b28a06fa8f
utkugun <utkugun@gmail.com>
2020-06-05 17:49:26 +0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import {Data} from './data'
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import{AngularFireModule} from'@angular/fire'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,AngularFireModule.initializeApp({
+    apiKey: "AIzaSyARA1LBVgGjs_XzbrJuzGdegptBGsa8lZA",
+    authDomain: "blistering-fire-8982.firebaseapp.com",
+    databaseURL: "https://blistering-fire-8982.firebaseio.com",
+    projectId: "blistering-fire-8982",
+    storageBucket: "blistering-fire-8982.appspot.com",
+    messagingSenderId: "638492837244",
+    appId: "1:638492837244:web:71dfcc26fd009643"
+  }) ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers:[]
